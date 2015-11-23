ea200d2eabc61ddbb699082a6be5a5f4d1f52ae3
autodidactic <shawnp1201@gmail.com>
2015-11-23 18:36:41 -0500

    Test message

---
+++ Backup/ReactJSNETwithMVC/App_Start/FilterConfig.cs
@@ -0,0 +1,14 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace ReactJSNETwithMVC
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());  
+            //this is where i add more tests
+        }
+    }
+}
